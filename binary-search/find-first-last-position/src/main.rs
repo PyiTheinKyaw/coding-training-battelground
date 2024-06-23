b94a2441fedb6710407d26fef351a742821c6ead
Ou Ou Yatlawkin <uutxeinkyaw@gmail.com>
2024-06-23 16:20:58 +0630

    DOne FFLP

--- binary-search/find-first-last-position/src/main.rs
+++ binary-search/find-first-last-position/src/main.rs
@@ -1,12 +1,10 @@
-use std::collections::HashMap;
-
 fn main() {
     // let nums = vec![5,7,7,8,8,10];
     // let nums = vec![2,2];
     // let nums = vec![1];
     // let nums = vec![1,1,2];
-    // let nums = vec![1,3];
-    let nums = vec![3,3,3];
+    let nums = vec![1,3];
+    // let nums = vec![3,3,3,3];
 
     let target = 3;
 
@@ -30,34 +28,50 @@ pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
 
         match nums[mid].cmp(&target) {
             Ordering::Equal => {
-                let mut is_previous_added = false;
-                let mut is_next_value_added = false;
-                let _cache_len = result.len();
 
-                if mid != 0 && nums.get(mid-1).is_some()
-                    && nums[mid] == nums[mid-1] {
-                    result.push((mid-1) as i32);
-                    is_previous_added = true;
-                }
+                let mut current_index = (mid % nums.len()) as i32;
 
-                if nums.get(mid+1).is_some() 
-                    && nums[mid] == nums[mid+1] {
+                while current_index - 1 >= 0 {
 
-                    if is_previous_added {
-                        result.push((mid+1) as i32);
-                    } else {
-                        result.push(mid as i32);
-                        result.push((mid+1) as i32);
+                    let previous_index = (current_index - 1) as usize % nums.len();
+                    if nums[current_index as usize] == nums[previous_index] {
+                        if result.len() == 1 {
+                            result.pop();
+                        }
+
+                        result.push(previous_index as i32);
                     }
-                    
-                    is_next_value_added = true;
+                    else { break; }
+
+                    current_index -= 1;
                 }
 
-                if (is_previous_added && !is_next_value_added) 
-                    || (_cache_len == result.len()) {
-                    result.push((mid) as i32);
+                if result.len() == 0 {
+                    result.push(mid as i32);
                 }
-            
+
+                current_index = (mid % nums.len()) as i32;
+
+                while current_index + 1 < nums.len() as i32 {
+
+                    let next_index = (current_index + 1) as usize % nums.len();
+
+                    if nums[current_index as usize] == nums[next_index] {
+                        if result.len() == 2 {
+                            result.pop();
+                        }
+
+                        result.push(next_index as i32);
+                    }
+                    else { break; }
+
+                    current_index += 1
+                }
+
+                if result.len() == 1 {
+                    result.push(mid as i32);
+                }
+
                 break;
             },
             Ordering::Greater => right = mid,
